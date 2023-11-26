af984d28f65218211f3d9ca2dca0c129b7ceb8ae
Jordan Hoare <jordan@pleiadesperspectives.com.au>
2023-11-26 11:15:47 +0800

    Init Snowflake feature repository

--- repository/common/entities.py
+++ repository/common/entities.py
@@ -2,6 +2,8 @@
 
 from feast import Entity, ValueType
 
+# Define an entity for the driver. You can think of an entity as a primary key used to
+# fetch features.
 driver = Entity(
     name="driver",
     join_keys=["driver_id"],

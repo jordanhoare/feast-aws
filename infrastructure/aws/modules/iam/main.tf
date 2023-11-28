11c53662007da6d5bc169b95f347781756c641d7
Jordan Hoare <jordan@pleiadesperspectives.com.au>
2023-11-28 16:13:23 +0800

    Configure aws_iam_openid_connect_provider.

--- infrastructure/aws/modules/iam/main.tf
+++ infrastructure/aws/modules/iam/main.tf
@@ -1,3 +1,13 @@
+resource "aws_iam_openid_connect_provider" "github_oidc" {
+  url             = "https://token.actions.githubusercontent.com"
+  client_id_list  = ["sts.amazonaws.com"]
+
+  thumbprint_list = [
+    "a031c46782e6e6c662c2c87c76da9aa62ccabd8e" 
+  ]
+}
+
+
 resource "aws_iam_role" "github_actions_role" {
   name = "github_actions_role"
 
@@ -8,19 +18,18 @@ resource "aws_iam_role" "github_actions_role" {
         Effect = "Allow",
         Action = "sts:AssumeRoleWithWebIdentity",
         Principal = {
-          Federated = "arn:aws:iam::432702836969:oidc-provider/token.actions.githubusercontent.com"
+          Federated = aws_iam_openid_connect_provider.github_oidc.arn
         },
         Condition = {
           "StringEquals": {
             "token.actions.githubusercontent.com:aud": "sts.amazonaws.com",
-            "token.actions.githubusercontent.com:sub": "repo:jordanhoare/feast-aws:*"
+            "token.actions.githubusercontent.com:sub": "repo:${var.repository_name}:*"
           }
         }
       }
     ]
   })
 }
-
 
 resource "aws_iam_role_policy" "github_actions_role" {
   name   = "github_actions_role"
--- infrastructure/aws/modules/iam/variables.tf
+++ infrastructure/aws/modules/iam/variables.tf
@@ -3,3 +3,11 @@ variable "aws_account_id" {
   type        = string
   default = "432702836969"
 }
+
+variable "repository_name" {
+  description = "The username and repository"
+  type        = string
+  default = "jordanhoare/feast-aws"
+}
+
+

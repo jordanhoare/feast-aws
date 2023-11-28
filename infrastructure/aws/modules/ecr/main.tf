47a329714fda79326ec035f2c221af4873710aeb
Jordan Hoare <jordan@pleiadesperspectives.com.au>
2023-11-28 20:34:28 +0800

    Formatting and linting.

--- infrastructure/aws/modules/ecr/main.tf
+++ infrastructure/aws/modules/ecr/main.tf
@@ -1,5 +1,5 @@
 resource "aws_ecr_repository" "ecr_repo" {
   name                 = var.repository_name
   image_tag_mutability = var.image_tag_mutability
-  force_delete = true
+  force_delete         = true
 }
